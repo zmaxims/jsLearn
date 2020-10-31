aa0ed52eef453d138aa68249454d8a0b50448d5b
Max <zmaxims@gmail.com>
2020-10-31 21:35:26 +0200

    PersonalMovieDB

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,8 @@
 "use strict";
 
-const numberOfFilms  =  +prompt(`Сколько  фильмов`,"");
+const numberOfFilms = +prompt(`Сколько  фильмов`, "");
 
-const  personalMovieDB = {
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
@@ -10,12 +10,30 @@ const  personalMovieDB = {
     private: false
 };
 
-const a = prompt(`Последний фильм`,"");
-const b = prompt("Ваша Оценка");
-const c = prompt(`Последний фильм`,"");
-const d = prompt("Ваша Оценка");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt(`Последний фильм`, "");
+    const b = prompt("Ваша Оценка");
 
+    if (a != null && b != null && a != '' && b != "" && b.length < 50) {
+        personalMovieDB.movies[a] = b;
+        else
+            i--;
+    }
+}
+
+if (personalMovieDB.count < 10 ){
+  Console.log("Мало фильмов"); 
+
+ else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    Console.log("Нормально фильмов");\
+    
+else if (personalMovieDB.count >= 30 ) {
+    console.log("Дохуя фильмов");
+else 
+console.log("Ошибка");  
+}
+ } 
+
+}
 console.log(personalMovieDB);
